dcb82fbb5c80abbd4f62f8cde5922fc805529d4a
Ethan B <ethanbinya@aol.com>
2018-04-15 16:53:20 -0400

    Replaced magic numbers with final variables.

--- src/View/View.java
+++ src/View/View.java
@@ -9,11 +9,13 @@ import javafx.stage.Stage;
 import java.awt.*;
 
 public class View extends Application {
+    private final String viewTitle = "Warehouse Management System";
+    private final String FXMLPath = "ProductPage.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("ProductPage.fxml"));
-        primaryStage.setTitle("Warehouse Management System");
+        Parent root = FXMLLoader.load(getClass().getResource(FXMLPath));
+        primaryStage.setTitle(viewTitle);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         // set scene's default size to be two-thirds of screens resolution
         primaryStage.setScene(new Scene(root, screenSize.getWidth()*2/3, screenSize.getHeight()*2/3));

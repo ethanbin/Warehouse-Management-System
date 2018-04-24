d3887a43987a872489442ef4470f0f2fb2411e30
Ethan B <ethanbinya@aol.com>
2018-04-24 00:31:54 -0400

    Added a code and message to DataControllerException

--- src/Exceptions/DataControllerException.java
+++ src/Exceptions/DataControllerException.java
@@ -10,13 +10,15 @@ public class DataControllerException extends Exception{
     // static publicly defined error codes
     public final static int	DATABASE_FAILED = 0;
     public final static int	PREPARED_STATEMENTS_FAILED = 1;
+    public final static int	EXPECTED_BOOLEAN_AS_INTEGER = 2;
 
     /**
      * Static publicly defined exception messages
      */
     public final static String[]  MESSAGE = {
             "The database failed to open for read/write",
-            "The SQL statements failed to prepare"
+            "The SQL statements failed to prepare",
+            "A boolean value of 0 or 1 was expected, but a different number was received"
     } ;
 
     protected int code;  // have this exception carry a code as well as

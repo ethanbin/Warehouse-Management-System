334677fafc3757828f3c32d241b8ed8537199bea
Ethan <ethanbinya@aol.com>
2018-05-03 13:07:46 -0400

    Controller now singleton, has final property SETTINGS_FILE_NAME with getter

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -2,16 +2,30 @@ package Controller;
 
 import java.sql.*;
 
-import Exceptions.DataControllerException;
 import View.View;
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
 
 /**
  * This class acts as the interface between the GUI view and the data models.
  */
 public class Controller {
+
+    private static Controller instance = null;
+    private final String SETTINGS_FILE_NAME = "settings";
+
+    private Controller(){
+
+    }
+
+    public static Controller getInstance(){
+        if (instance == null)
+            instance = new Controller();
+        return instance;
+    }
+
+    public String getSettingsFileName() {
+        return SETTINGS_FILE_NAME;
+    }
 
     public static void main(String[] args) {
         DataController.getInstance();

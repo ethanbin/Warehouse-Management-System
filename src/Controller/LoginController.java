23f8c21e73b72b4e0e08de3012f45b247c18e312
Ethan <ethanbinya@aol.com>
2018-05-03 19:04:33 -0400

    Logs in only if username/pw credentials correct

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
@@ -8,8 +9,8 @@ import javafx.scene.control.TextField;
 
 public class LoginController {
     //References to our objects and fields create in SceneBuilder.
-    @FXML private TextField UsernameField;
-    @FXML private TextField PasswordField;
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
 
     SceneController sceneController;
 
@@ -19,14 +20,24 @@ public class LoginController {
 
     //Methods to be called by events in GUI
     @FXML
-    protected void loginAction(ActionEvent event) throws Exception    {
-        System.out.println("User logged in.");
-        sceneController.activate("Home");
+    protected void loginAction(ActionEvent event) throws Exception {
+        if (MainController.getInstance().loginUser(usernameField.getText(), passwordField.getText())) {
+            System.out.println("User logged in.");
+            sceneController.activate("Home");
+        }
+        else
+            System.out.println("Incorrect username/password.");
     }
 
     @FXML
     protected void quitAction(ActionEvent event)    {
         Platform.exit();
     }
-}
+
+    @FXML
+    void initialize() {
+        assert usernameField != null : "fx:id=\"usernameField\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+        assert passwordField != null : "fx:id=\"passwordField\" was not injected: check your FXML file 'LoginScreen.fxml'.";
+
+    }}
 

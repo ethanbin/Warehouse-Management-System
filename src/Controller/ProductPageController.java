62187f6b9bc17cecd4863f80ae974eb47466a2be
Gabriel Batista <gabedbatista@gmail.com>
2018-05-06 22:27:04 -0400

    aligned table with input containers

--- src/Controller/ProductPageController.java
+++ src/Controller/ProductPageController.java
@@ -11,6 +11,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 

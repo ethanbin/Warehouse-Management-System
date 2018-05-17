33829ad916c47d24664b905e81ca7804518c08da
Ethan B <ethanbinya@aol.com>
2018-05-16 23:07:38 -0400

    Updated javadoc for User class

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,5 +1,10 @@
 package Model;
 
+/**
+ * A simple model for a User's data as pulled from a database. This model contains
+ * all the data of a User from the database with the exception of the user's
+ * password to prevent potential security vulnerabilities from memory scanners.
+ */
 public class User {
     private int user_id;
     private String name;
@@ -8,13 +13,12 @@ public class User {
     private int warehouseID;
 
     /**
-     * The User data model contains all the data of a record from the User table of the database
-     * except for the password to prevent potential security vulnerabilities to memory scanning software.
-     * @param user_id ID of the user
+     * Constructor which sets every property in the class.
+     * @param user_id integer ID of the user
      * @param name full name of the user
      * @param isAdmin admin status of user
      * @param username username
-     * @param warehouseID Database ID of the warehouse this user is employed at
+     * @param warehouseID integer ID of the warehouse this user is employed at
      */
     public User(int user_id, String name, boolean isAdmin, String username, int warehouseID) {
         this.user_id = user_id;
@@ -24,22 +28,42 @@ public class User {
         this.warehouseID = warehouseID;
     }
 
+    /**
+     * Return User ID.
+     * @return User ID
+     */
     public int getUser_id() {
         return user_id;
     }
 
+    /**
+     * Return the User's full name.
+     * @return the User's full name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Return true if the user is an admin.
+     * @return true if the user is an admin
+     */
     public boolean isAdmin() {
         return isAdmin;
     }
 
+    /**
+     * Return the username.
+     * @return the username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Return the ID of the warehouse where the user is employed.
+     * @return the ID of the warehouse where the user is employed
+     */
     public int getWarehouseID() {
         return warehouseID;
     }
